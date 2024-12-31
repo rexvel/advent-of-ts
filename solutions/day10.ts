6f434d0fb49f82ede6ea241fd4e77fbd27e14bb0
rexvel <rexvelele@gmail.com>
2024-12-31 20:46:46 +0200

    feat: add day 10 solution

---
+++ solutions/day10.ts
@@ -0,0 +1,15 @@
+enum Gift {
+    Coal = 0b00000,                        
+    Train = 0b00001 << 0b00000,                   
+    Bicycle = 0b00001 << 0b00001,                 
+    SuccessorToTheNintendoSwitch = 0b00001 << 0b00010,
+    TikTokPremium = 0b01000,           
+    Vape = 0b00001 << 0b00100,                     
+  
+    Traditional = Train | Bicycle,   
+    
+    OnTheMove = Coal | Bicycle | TikTokPremium | Vape, 
+    
+    OnTheCouch = (Coal | TikTokPremium | Vape | SuccessorToTheNintendoSwitch) & ~Bicycle
+  }
+  
